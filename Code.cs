ee28d8758379bdebf005acbfa4ce7ef5574f21de
Pik6C <151807901+Pik6C@users.noreply.github.com>
2024-04-08 17:36:36 +0900

    Update Code.cs
    Read.FileLine method is no longer asynchronous
    Now returns the contents of the file line by line

--- Code.cs
+++ Code.cs
@@ -3,24 +3,26 @@ using System.IO;
 using System.Text;
 using System.Drawing;
 using System.Threading.Tasks;
+using System.Collections.Generic;
 namespace Load;
 
 public class Read
 {
   public static string File(string File , string Character_CODE)
   {
-    StreamReader sr = new StreamReader(File, Encoding.GetEncoding(Character_CODE));
-    string str = sr.ReadToEnd();
-    sr.Close();
-    return str;
+    using(var sr = new StreamReader(File, Encoding.GetEncoding(Character_CODE))){
+     return sr.ReadToEnd(); 
     }
-  public static async void FileLine(string File , string Character_CODE)
-  {
-    StreamReader sr = new StreamReader(File, Encoding.GetEncoding(Character_CODE));
-    while(sr.Peek() != -1){
-    var das = await sr.ReadLineAsync();
-      Console.WriteLine(das);
-    }
-    sr.Close();
-  }
 }
+  public static IEnumerable<string> FileLine(string File , string Character_CODE)
+  {
+    using var sr = new StreamReader(File, Encoding.GetEncoding(Character_CODE));
+      
+      while(!sr.EndOfStream){
+        yield return sr.ReadLine();
+    }
+        
+  }
+}  
+
+
